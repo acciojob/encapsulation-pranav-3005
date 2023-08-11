bb1717ff86e061d2c8b9e94aba17512405581157
pranav <pranavanandth19@gamil.com>
2023-08-11 09:45:14 +0530

    1st attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
-  
+  public static void main(String args[])
+  {
+      RWOnly obj=new RWOnly();
+      Scanner sc=new Scanner(System.in);
+      String name=sc.nextLine();
+      obj.setter(name);
+      System.out.println(obj.getter());
+  }
 }
